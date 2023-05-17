8ca3c4516ccafd42ed3789969653a1dfbf4b3a7b
Sander van de Vondervoort <s.vondervoort@gmail.com>
2023-05-17 11:34:05 +0200

    Nextjs to Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Nested from './Nested.svelte';
+</script>
+
+<div class="container mx-auto py-8">
+    <div class="divide-y divide-slate-400/20 rounded-lg bg-white text-[0.8125rem] leading-5 text-slate-900 shadow-xl shadow-black/5 ring-1 ring-slate-700/10">
+        <div class="p-4"></div>
+        <div class="p-4"></div>
+        <div class="p-4"></div>
+    </div>
+</div>
+
+<style lang="postcss">
+
+</style>
